00aa67ecc697cde752ae39c51809a512162edf7d
Melissaf M <melissam200604@gmail.com>
2025-05-08 17:01:28 -0500

    validaciones en schema de actividades

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -16,7 +16,7 @@ const taskSchema = new mongoose.Schema(
     },
     place: {  // Nuevo campo: lugar de la actividad
       type: String,
-      required: false,
+      required:true,
     },
     responsible: {  // Nuevo campo: responsables
       type: [String], // Array de strings para múltiples responsables
--- src/schemas/task.schema.js
+++ src/schemas/task.schema.js
@@ -4,10 +4,11 @@ export const createTaskSchema = z.object({
 
   title: z.string({
     required_error: "Title is required",
-  }),
-  
+  }) .max(90),
   description: z.string().optional(),
   date: z.string().optional(),
-  place: z.string().optional(), // Nuevo campo
-  responsible: z.array(z.string()).optional(), // Nuevo campo
+  place: z.string({
+    required_error: "place is required",
+  }) .max(30),
+  responsible: z.array(z.string().max(70)).optional(), 
 });
