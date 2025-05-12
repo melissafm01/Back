348a06cca290474b2b8814579a17e1cc7f4792fb
Melissaf M <melissam200604@gmail.com>
2025-05-12 09:13:56 -0500

    configuracion en el controller

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -2,7 +2,7 @@ import Task from "../models/task.model.js";
 
 export const getTasks = async (req, res) => {
   try {
-    const tasks = await Task.find({ user : req.user.id }).populate("user");
+    const tasks = await Task.find({ user : req.user.id }).populate("user", "email _id");
     res.json(tasks);
   } catch (error) {
     return res.status(500).json({ message: error.message });
