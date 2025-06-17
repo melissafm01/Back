27a2e339542cb354b74788385cb8af397c73cae7
JuanRengifo <rrengifoa@gmail.com>
2025-06-17 08:46:19 -0500

    actualizacion de rama - melissa

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,7 +7,9 @@ import {
   register,
   verifyToken,
   registerAdmin,
-  createInitialSuperAdmin // <- Crear super admin inicial
+  createInitialSuperAdmin , // <- Crear super admin inicial
+ loginWithGoogle,
+ sendPasswordResetEmail
 } from "../controllers/auth.controller.js";
 
 import { authorizeRoles } from "../middlewares/role.middleware.js";
@@ -29,5 +31,6 @@ router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
 router.get("/verify", verifyToken);
 router.post("/logout",  logout);
-
+router.post("/google", loginWithGoogle);
+router.post("/password-reset", sendPasswordResetEmail);
 export default router;
