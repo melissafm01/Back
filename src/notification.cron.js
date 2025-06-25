cfb53574213f6d96d6dab9f37ae0498461a22fc7
JuanRengifo <rrengifoa@gmail.com>
2025-06-25 08:54:48 -0500

    //

--- src/notification.cron.js
+++ src/notification.cron.js
@@ -186,7 +186,7 @@ export const startNotificationCron = (io) => {
   
   // Para pruebas: ejecutar cada minuto
   // Para producción: cambiar a "0 1 * * *" (1am diariamente)
-  const cronPattern = "0 7 * * *"; // Ejecutar a las 7am diariamente
+  const cronPattern = "0 1 * * *"; // Ejecutar a las 7am diariamente
   
   cron.schedule(cronPattern, () => {
     console.log(`\n⏰ CRON ACTIVADO: ${dayjs().format('YYYY-MM-DD HH:mm:ss')}`);
